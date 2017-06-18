269515ea5fcb0ef33f74be4c1fc2a33a684362b9
PowerLemons <f0kent0sh@gmail.com>
2017-06-18 12:10:45 -0300

    Latest donation added

--- javascript/balancebar.js
+++ javascript/balancebar.js
@@ -1,11 +1,23 @@
 $.ajax({
-	url: "https://chain.so/api/v2/get_address_balance/DOGE/DFundmtrigzA6E25Swr2pRe4Eb79bGP8G1/500",
+	url: "https://chain.so/api/v2/get_address_balance/DOGE/DQo4JM5X5Jmbhr83p8s1nAAkEuR6ipkRhu/500",
 	dataType: "text",
 	success: function(data) {
 		var json = $.parseJSON(data);
 		var balance = json.data.confirmed_balance;
 
-		$(".progress-bar").animate({width: (balance / 15000000 * 100).toString() + "%"}, 2000);
+		$(".progress-bar").animate({width: (balance / 1500000 * 100).toString() + "%"}, 2000);
 		$('.balance').html("&ETH;" + Math.round(balance).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " of &ETH;1,500,000 donated so far!");
+	}
+});
+
+$.ajax({
+	url: "https://chain.so/api/v2/address/DOGE/DQo4JM5X5Jmbhr83p8s1nAAkEuR6ipkRhu",
+	dataType: "text",
+	success: function(data) {
+		var json = $.parseJSON(data);
+		var donation = json.data.txs[0].incoming.value;
+		var address = json.data.txs[0].incoming.inputs[0].address;
+		
+		$('.latest-donation').html("<b>LATEST DONATION:</b> &ETH;" + (Math.round(donation * 1000) / 1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " by " + address);
 	}
 });
